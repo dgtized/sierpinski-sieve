185b205945ccdf23b1732a864c30aff86737940c
Charles Comstock <dgtized@gmail.com>
2017-04-02 15:28:28 -0500

    extract bit blit routines into their own namespace
    For size=325, seeing
    350ms for blit routines
    255ms for paint routines

---
+++ src/sierpinski_sieve/blit.cljs
@@ -0,0 +1,23 @@
+(ns sierpinski-sieve.blit)
+
+;; tried to use putImageData to blit out data
+;; doesn't seem to be significantly faster
+
+(defn paint-row [pixel-data y row size]
+  (let [y-offset (* y size)]
+    (doseq [[x value] (map-indexed vector row)]
+      (let [disp-x (+ (int (* (- size y) 0.5)) x)
+            p (* (+ y-offset disp-x) 4)]
+        (if (odd? value)
+          (aset pixel-data (+ p 3) 0))))))
+
+(defn paint [canvas-id size sieve]
+  (when-let [canvas-element (. js/document getElementById canvas-id)]
+    (when-let [ctx (.getContext canvas-element "2d")]
+      (let [pixels (.createImageData ctx size size)
+            pixel-data (.-data pixels)]
+        (doseq [i (range 0 (* size size))]
+          (aset pixel-data (+ (* i 4) 3) 255))
+        (doseq [[y row] (map-indexed vector sieve)]
+          (paint-row pixel-data y row size))
+        (.putImageData ctx pixels 0 0)))))
--- src/sierpinski_sieve/core.cljs
+++ src/sierpinski_sieve/core.cljs
@@ -1,6 +1,7 @@
 (ns sierpinski-sieve.core
   (:require [devtools.core :as devtools]
-            [reagent.core :as reagent :refer [atom]]))
+            [reagent.core :as reagent :refer [atom]]
+            [sierpinski-sieve.blit :as blit]))
 
 (enable-console-print!)
 
@@ -37,25 +38,6 @@
           (paint-row ctx y row size))
         (.fill ctx)))))
 
-(defn blit-row [pixel-data y row size]
-  (let [y-offset (* y size)]
-    (doseq [[x value] (map-indexed vector row)]
-      (let [disp-x (+ (int (* (- size y) 0.5)) x)
-            p (* (+ y-offset disp-x) 4)]
-        (if (odd? value)
-          (aset pixel-data (+ p 3) 0))))))
-
-(defn blit [canvas-id size sieve]
-  (when-let [canvas-element (. js/document getElementById canvas-id)]
-    (when-let [ctx (.getContext canvas-element "2d")]
-      (let [pixels (.createImageData ctx size size)
-            pixel-data (.-data pixels)]
-        (doseq [i (range 0 (* size size))]
-          (aset pixel-data (+ (* i 4) 3) 255))
-        (doseq [[y row] (map-indexed vector sieve)]
-          (blit-row pixel-data y row size))
-        (.putImageData ctx pixels 0 0)))))
-
 (defn render-canvas [size]
   (fn []
     [:center [:canvas {:width size :height size :id "canvas"}]]))
@@ -63,7 +45,7 @@
 (defn main [size]
   (reagent/render [(render-canvas size)]
                   (. js/document (getElementById "app")))
-  (time (blit "canvas" size (take size (sieve '(1) 2))))
+  (time (blit/paint "canvas" size (take size (sieve '(1) 2))))
   (time (paint "canvas" size (take size (sieve '(1) 2)))))
 
 (main 325)

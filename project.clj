4ddb6f19092213bf4ae9a91531808f9367cb562c
Charles Comstock <dgtized@gmail.com>
2017-09-18 20:47:34 -0500

    clojure 1.9.0-beta1

--- project.clj
+++ project.clj
@@ -6,7 +6,7 @@
 
   :min-lein-version "2.7.1"
 
-  :dependencies [[org.clojure/clojure "1.9.0-alpha17"]
+  :dependencies [[org.clojure/clojure "1.9.0-beta1"]
                  [org.clojure/clojurescript "1.9.908"]
                  [org.clojure/core.async "0.3.443"
                   :exclusions [org.clojure/tools.reader]]

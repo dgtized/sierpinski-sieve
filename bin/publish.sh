7356f899aa7aefc018887439cf6d8c4898dfe8f0
Charles Comstock <dgtized@gmail.com>
2021-01-03 17:02:14 -0600

    upgrade deploy script

--- bin/publish.sh
+++ bin/publish.sh
@@ -1,11 +1,22 @@
 #!/bin/bash -e
 
+function cleanup() {
+    rm -rf resources/public/{.git,js} target
+}
+
+cleanup
+
 rm -rf resources/public/.git
 clojure -m figwheel.main --build-once release
 
+mkdir -p resources/public/js
+cp target/public/js/sierpinsky_sieve.* resources/public/js
+
 pushd resources/public
 git init
-git add .
+git add index.html js/* css/*
 git commit -m "Deploy to GitHub Pages"
 git push --force --quiet "git@github.com:dgtized/sierpinski-sieve.git" master:gh-pages
 popd
+
+cleanup

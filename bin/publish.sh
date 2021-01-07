d72e9e9bfac519be90df72afacf4b58417b2ee37
Charles Comstock <dgtized@gmail.com>
2021-01-07 04:30:30 -0600

    Push to gh-pages directly

--- bin/publish.sh
+++ bin/publish.sh
@@ -13,9 +13,10 @@ cp target/public/js/sierpinsky_sieve.* resources/public/js
 
 pushd resources/public
 git init
+git checkout -b gh-pages
 git add index.html js/* css/*
 git commit -m "Deploy to GitHub Pages"
-git push --force --quiet "git@github.com:dgtized/sierpinski-sieve.git" master:gh-pages
+git push --force --quiet "git@github.com:dgtized/sierpinski-sieve.git" gh-pages:gh-pages
 popd
 
 cleanup
